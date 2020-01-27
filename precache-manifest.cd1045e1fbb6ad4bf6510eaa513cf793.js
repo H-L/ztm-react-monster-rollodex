cae4f4c3ee45c7248e8e78f6b036b5ad59e8eff1
H-L <hadrien.lepoutre@gmail.com>
2020-01-27 16:56:48 +0100

    Updates

---
+++ precache-manifest.cd1045e1fbb6ad4bf6510eaa513cf793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e585aa9b931497db3a094c3d39bec7ab",
+    "url": "/ztm-react-monster-rollodex/index.html"
+  },
+  {
+    "revision": "1f9b3442c4a3c7dfb778",
+    "url": "/ztm-react-monster-rollodex/static/css/main.66727472.chunk.css"
+  },
+  {
+    "revision": "7d5552132026b560232d",
+    "url": "/ztm-react-monster-rollodex/static/js/2.975d0e20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ztm-react-monster-rollodex/static/js/2.975d0e20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f9b3442c4a3c7dfb778",
+    "url": "/ztm-react-monster-rollodex/static/js/main.1d4862a9.chunk.js"
+  },
+  {
+    "revision": "8de9a72af7877d45bf30",
+    "url": "/ztm-react-monster-rollodex/static/js/runtime-main.5bcbf119.js"
+  }
+]);
